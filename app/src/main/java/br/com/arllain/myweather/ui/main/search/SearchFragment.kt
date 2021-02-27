ecf7c19f0f27b9bf4e62a5e813fd56d5610604fa
Arllain <arllain@gmail.com>
2021-02-27 12:15:49 -0300

    Permissao de acesso network, ContextExtension function

--- app/src/main/java/br/com/arllain/myweather/ui/main/search/SearchFragment.kt
+++ app/src/main/java/br/com/arllain/myweather/ui/main/search/SearchFragment.kt
@@ -4,8 +4,11 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import br.com.arllain.myweather.databinding.FragmentSearchBinding
+import br.com.arllain.myweather.extension.isInternetAvailable
+
 
 class SearchFragment: Fragment() {
 
@@ -19,4 +22,18 @@ class SearchFragment: Fragment() {
         binding = FragmentSearchBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnSearch.setOnClickListener {
+            if (requireContext().isInternetAvailable()){
+                Toast.makeText(requireContext(), "Has internet", Toast.LENGTH_SHORT).show()
+            }else {
+                Toast.makeText(requireContext(), "Has no internet", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+
+
 }
